3fa3d587d8fddffff97caee453410af37a2483c7
Maija Rikala <163442056+MaijaRikala@users.noreply.github.com>
2026-01-28 10:10:27 +0200

    added functions to GameManager
    Awake, RestartScene and QuitGame functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,43 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
+    // Class variables -----------------------------------------------
+
+    // Static private variable to hold the reference
     private static GameManager _instance;
-
-    public static GameManager instance;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Public reference for other classes (only get, not set)
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("ERROR: no GamrManager exists");
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+    // Initialize ----------------------------------------------------
+
+    // Initialize variables, in this case only _instance
+    // Set _instance reference as soon as possible
+    private void Awake() => _instance = this;
+
+
+
+    // Scene control -------------------------------------------------
+
+    // Simple restart function for the scene
+    public void RestartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+
+
+    // Quit game -----------------------------------------------------
+
+    public void QuitGame() => Application.Quit();
+
 }
