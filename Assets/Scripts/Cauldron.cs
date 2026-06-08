05a0be2059779259760afa800a2fa7f87876cf94
Maija Rikala <163442056+MaijaRikala@users.noreply.github.com>
2026-06-08 18:11:15 +0300

    added ingredient prefab and modified cauldron script
    - fixed changing temperature and consistency of cauldron
    - modified adding ingredient to cauldron

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -10,7 +10,7 @@ public class Cauldron : MonoBehaviour
     public static Cauldron Instance { get; private set; }
 
     // List for ingredients
-    private List<GameObject> Ingredients = new List<GameObject>();
+    private List<string> Ingredients = new List<string>();
     // Values describing the potion
     // TODO: check that variable names make sense
     private int Temperature;
@@ -56,20 +56,22 @@ public class Cauldron : MonoBehaviour
 
 
     // Called, when player adds an ingredient
-    void addIngredient(GameObject ingredient)
+    void addIngredient(Ingredient ingredient)
     {
-        Ingredients.Add(ingredient);
+        Ingredients.Add(ingredient.ingredientName); // Add only name of the ingredient to list
+        changeTemperature(ingredient.temperature);
+        changeConsistency(ingredient.consistency);
     }
 
     // Called, when player changes temperature of the potion
     void changeTemperature(int temperature)
     {
-        this.Temperature = temperature;
+        this.Temperature = Temperature + temperature;
     }
 
     // Called, when player stirs the potion
     void changeConsistency(int consistency)
     {
-        this.Consistency = consistency;
+        this.Consistency = Consistency + consistency;
     }
 }
